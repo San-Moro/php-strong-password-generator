7e1c26a6bdceb512daa00730d5fa497f75e11a5b
San-Moro <valentinacortese.dsg@gmail.com>
2022-12-09 10:37:04 +0100

    milestone1 correction

--- index.php
+++ index.php
@@ -4,37 +4,33 @@ Una nostra funzione utilizzerà questo dato per generare una password casuale (c
 Scriviamo tutto (logica e layout) in un unico file *index.php* -->
 
 <?php
-$numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-$uppercase_letters = ["A", "B", "C", "D", "E"," F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-$lowercase_letters = ["a", "b", "c", "d", "e"," f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-$simbols = ["!", "£", "$", "%", "&"," ?", "*", "+", "@", "#", "-", "_", "~"];
+$numbers = "0123456789";
+$uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+$lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
+$simbols = "!£$%&?*+@#-_~/<>()[]{}";
+$full_chars = $numbers.$uppercase_letters.$lowercase_letters.$simbols;
+
+if (isset($_GET["password-length"])) {
+    $password_length = $_GET["password-length"];
+    $result = "";
+
+    if ($password_length >= 8 && $password_length <= 35) {
+        $password = "";
+
+        while (strlen($password) < $password_length) {
+            $index = rand(0, (strlen($full_chars) - 1));
+            $char = $full_chars[$index];
+            $password .= $char;
+        }
+        $result = $password;
+    }
+}
 
 // funzione che genera un numero random,
 // in base al numero random prendo dall'array l'elemento (numero random = indice elemento),
 // pusho l'elemento in un nuovo array password items,
 // ripeto per ogni array,
 // ripeto tutto n. volte = password_length,
-
-
-
-function generatePasswordItems($numbers, $uppercase_letters, $lowercase_letters, $simbols ) {
-    $rnd_number = rand(0, 9);
-    $password_items[] = $numbers["$rnd_number"];
-    $rnd_number = rand(0, 25);
-    $password_items[] = $uppercase_letters["$rnd_number"];
-    $rnd_number = rand(0, 25);
-    $password_items[] = $lowercase_letters["$rnd_number"];
-    $rnd_number = rand(0, 12);
-    $password_items[] = $simbols["$rnd_number"];
-    var_dump($password_items);
-}
-
-$password_length = $_GET["password-length"] ?? "";
-if(!empty($password_length)) {
-    echo "genero password";
-
-    generatePasswordItems($numbers, $uppercase_letters, $lowercase_letters, $simbols);
-}
 ?>
 
 <!DOCTYPE html>
@@ -50,12 +46,17 @@ if(!empty($password_length)) {
         <div class="container">
             <form action="index.php" method="GET">
                 <label for="password-length">Lunghezza Password: </label>
-                <input type="text" id="password-length" name="password-length">
+                <input type="number" id="password-length" name="password-length">
                 <div>
                     <button type="submit">Invia</button>
                     <button type="reset">Annulla</button>
                 </div>
             </form>
+            <?php if(isset($result)) { ?>
+            <section>
+                <p><?php echo $result ?></p>
+            </section>
+            <?php } ?>
         </div>
     </main>
 </body>
